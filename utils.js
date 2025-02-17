ee59ac2c76bf57aa701da1488269f3736d1109b7
袁瑶琪 <yuanyaoqi@mac.local>
2025-02-17 22:06:27 +0800

    add emily

--- utils.js
+++ utils.js
@@ -1,4 +1,14 @@
-function formatArray(i) {
+function formatDove() {
+    let rows = 150
+    return {
+        rows,
+        specialLine: [8, 13, 22, 27, 40, 45, 54, 59],
+        cols: 30,
+        gridData: Array(rows).fill(0).map((_, i) => formatDoveLine(i))
+    }
+}
+
+function formatDoveLine(i) {
     let chart1 = new Array(8);
     let chart2 = new Array(13);
     if (i % 6 == 2) {
@@ -15,7 +25,7 @@ function formatArray(i) {
     let sChart1 = formatChartS((i + 5) % 20)
     let sChart2 = formatChartS((i + 15) % 20)
     let ret = [].concat(chart1, diffChart, sChart1, diffChart, chart2, diffChart, sChart2, diffChart, chart1);
-    return ret;
+    return ret.slice(0, Math.floor(i / 2) + 1);
 }
 
 function formatChartS(_i) {
@@ -47,4 +57,39 @@ function formatChartS(_i) {
         diffChart2[1] = "B";
     }
     return diffChart2
+}
+
+function formatEmily() {
+    let data = [
+        'k k yo k yo k p v',
+        'p p p p p p k m1l p',
+        'k k k yo k yo k k p p v',
+        'p p p p p p p p k m1l k p',
+        'k k k k yo k yo k k k p B p v',
+        'p p p p p p p p p p k m1l k k p',
+        'k k k k k yo k yo k k k k p p p p v',
+        'p p p p p p p p p p p p k m1l k k k p',
+        'k k2tog k k k k k k k ssk p B p p B v',
+        'p p p p p p p p p p k m1l k k k k p',
+        'k k2tog k k k k k ssk p p p p p p v',
+        'p p p p p p p p k m1l k k p k k p',
+        'k k2tog k k k ssk p p p p k p p v',
+        'p p p p p p k k k k p k k p',
+        'k k2tog k ssk p p p p k p B v',
+        'p p p p k k k k p k k p',
+        'k sssk p p p p k p p v',
+        'bo bo bo bo k p k k p',
+    ]
+    return {
+        rows: 20,
+        cols: 20,
+        specialLine: [10],
+        gridData: data.map((str, index) => {
+            let row = str.split(' ')
+            // 计算需要填充的空格数
+            const padding = 19 - row.length
+            // 在数组开头添加空值来实现右对齐
+            return [index+1].concat(new Array(padding).fill(null).concat(row))
+        })
+    }
 }
